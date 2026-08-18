579e5991bb919969d71aad93ce178aa4dbdf6c7c
Diego Mena <diegomenahs04@gmail.com>
2026-08-18 14:31:50 -0600

    Modificacion Class Calendario

--- src/Logic/Calendario.java
+++ src/Logic/Calendario.java
@@ -2,10 +2,11 @@ package Logic;
 
 public class Calendario {
     private Recurso[][] mRecursos;
-    private String[7] dias = {"Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"}; //se crea el vector de dias para la Matriz
+    private final String[] dias = {"Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"}; //se crea el vector de dias para la Matriz
     Calendario(){
-        for(int i = 0; i<dias.length();i++)
+        for(int i = 0; i<dias.length;i++){
 
+        }
 
 
 
